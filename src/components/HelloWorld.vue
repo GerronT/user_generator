27b9888b3c878368389e95a4b1dac75c9685d7eb
Gerron Tinoy <regnortony@outlook.com>
2019-11-02 14:37:01 +0000

    fixed formatting

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,31 +1,34 @@
 <template>
   <div class="hello">
-    <!--<img v-bind:src='picture' alt="user_pic">-->
     <input type="text" placeholder="Enter number of users (max: 5000)">
     <input type="submit" value="Generate">
+
     <h1>Number of Generated Users: {{length}}</h1>
-    <p>Sort:</p>
-    <select dir="rt1" id="sort" name="sort">
-      <option value="" disabled selected>Select</option>
-      <option value="alphabetical-username">By Username</option>
+
+    <p class='label'>Sort:</p>
+    <select id="sort" name="sort">
+      <option value="" disabled selected>----- Select -----</option>
+      <option value="alphabetical-username-az">By Username (A-Z)</option>
+      <option value="alphabetical-username-za">By Username (Z-A)</option>
     </select>
+
     <table class="table">
-            <thead>
-                <tr>
-                    <th v-for="(column, index) in columns" :key="index"> {{column}}</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="(item, index) in items" :key="index">
-                    <td>{{index + 1}}</td>
-                    <td>{{ item.title }}</td>
-                    <td>{{ item.firstName }}</td>
-                    <td>{{ item.lastName }}</td>
-                    <td>{{ item.userName }}</td>
-                    <td><img v-bind:src='item.photo' alt="user_pic"></td>
-                </tr>
-            </tbody>
-        </table>
+      <thead>
+        <tr>
+          <th v-for="(column, index) in columns" :key="index"> {{column}}</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(item, index) in items" :key="index">
+          <td>{{index + 1}}</td>
+            <td>{{ item.title }}</td>
+            <td>{{ item.firstName }}</td>
+            <td>{{ item.lastName }}</td>
+            <td>{{ item.userName }}</td>
+            <td><img v-bind:src='item.photo' alt="user_pic"></td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
@@ -36,104 +39,108 @@ export default {
   data () {
     return {
       items: [],
-      length: null,
+      length: 10,
       columns: ['#', 'Title', 'First Name', 'Last Name', 'Username', 'Photo']
     }
   },
   mounted () {
     if (this.length > 0) {
-    axios.get('https://randomuser.me/api/?results=' + this.length)
-    .then(response => (
-      this.items = response.data.results.map(function (item, index) {
-        let temp = {
-          ID: index + 1,
-          title: item.name.title,
-          firstName: item.name.first,
-          lastName: item.name.last,
-          userName: item.login.username,
-          photo: item.picture.large
-        };
-        return temp;
-      })
+      axios.get('https://randomuser.me/api/?results=' + this.length).then(response => (
+        this.items = response.data.results.map(function (item, index) {
+          let temp = {
+            ID: index + 1,
+            title: item.name.title,
+            firstName: item.name.first,
+            lastName: item.name.last,
+            userName: item.login.username,
+            photo: item.picture.large
+          };
+          return temp;
+        })
       ))
     }
   }
 }
-</script>
 
+</script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  h3 {
+    margin: 40px 0 0;
+  }
 
-table {
-  font-family: arial, sans-serif;
-  border-collapse: collapse;
-  width: 100%;
-}
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
 
-td, th {
-  border: 1px solid #dddddd;
-  text-align: left;
-  padding: 8px;
-}
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
 
-tr:nth-child(even) {
-  background-color: #dddddd;
-}
+  a {
+    color: #42b983;
+  }
 
-input[type=text]{
-  width: 30%;
-  padding: 12px 20px;
-  margin: 8px 0;
-  display: inline-block;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  box-sizing: border-box;
-}
+  table {
+    font-family: arial, sans-serif;
+    border-collapse: collapse;
+    width: 100%;
+  }
 
-select {
-  width: 15%;
-  padding: 5px 5px;
-  margin: 8px 0;
-  display: inline-block;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  box-sizing: border-box;
-  margin-left: 5px;
-}
+  td, th {
+    border: 1px solid #dddddd;
+    text-align: left;
+    padding: 8px;
+  }
 
-p {
-  font-weight: 700;
-  font-size: 15px;
-  display: inline-block;
-}
+  tr:nth-child(even) {
+    background-color: #dddddd;
+  }
 
-input[type=submit] {
-  width: 10%;
-  background-color: #4CAF50;
-  color: white;
-  padding: 14px 20px;
-  margin: 8px 0;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-  margin-left: 10px;
-}
+  input[type=text]{
+    width: 30%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+  }
 
-input[type=submit]:hover {
-  background-color: #45a049;
-}
+  select {
+    width: 15%;
+    padding: 5px 5px;
+    margin: 8px 0;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+    margin-left: 5px;
+    float: left;
+    display: inline-block;
+  }
+
+  p[class=label] {
+    font-weight: 700;
+    font-size: 15px;
+    float: left;
+    display: inline-block;
+  }
+
+  input[type=submit] {
+    width: 10%;
+    background-color: #4CAF50;
+    color: white;
+    padding: 14px 20px;
+    margin: 8px 0;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+    margin-left: 10px;
+  }
+
+  input[type=submit]:hover {
+    background-color: #45a049;
+  }
 </style>
